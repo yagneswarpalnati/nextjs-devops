f9a412f07403099ec3bed39b69b3a21935757cad
yagneswarpalanti <palnatiyagneswar25@gmail.com>
2026-03-27 06:56:31 +0000

    tag

--- app/page.js
+++ app/page.js
@@ -16,7 +16,7 @@ export default function Home() {
         <div className={styles.intro}>
           <h1>Next.js Static Site</h1>
           <p>Welcome to the Next.js Static Site!</p>
-          <p>Test 7</p>
+          <p>Tag</p>
         </div>
         <div className={styles.ctas}>
           <a
